d4f636410d8338b16874c58606f6ad7bcd0f40d8
George <71434803+george-swift@users.noreply.github.com>
2021-10-11 20:55:12 +0100

    Add implementation of heap sort and insert sort to components

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
 const ButtonPanel = ({
-  randomize, merge, quick, sorted,
+  randomize, insertion, merge, quick, heap, sorted,
 }) => {
   const [visible, setVisible] = useState(false);
 
@@ -17,7 +17,7 @@ const ButtonPanel = ({
   }, [sorted]);
 
   return (
-    <div className="button-panel">
+    <div className="container button-panel">
       <button
         type="button"
         className="btn vsx-btn"
@@ -29,6 +29,16 @@ const ButtonPanel = ({
       <button
         type="button"
         className="btn vsx-btn"
+        title="Worst case: O(N^2)"
+        onClick={() => startSorting(insertion)}
+        disabled={visible}
+      >
+        Insertion Sort
+      </button>
+      <button
+        type="button"
+        className="btn vsx-btn"
+        title="Worst case: O(NLogN)"
         onClick={() => startSorting(merge)}
         disabled={visible}
       >
@@ -37,10 +47,20 @@ const ButtonPanel = ({
       <button
         type="button"
         className="btn vsx-btn"
+        title="Worst case: O(N^2)"
         onClick={() => startSorting(quick)}
         disabled={visible}
       >
         Quick Sort
+      </button>
+      <button
+        type="button"
+        className="btn vsx-btn"
+        title="Worst case: O(NLogN)"
+        onClick={() => startSorting(heap)}
+        disabled={visible}
+      >
+        Heap Sort
       </button>
     </div>
   );
@@ -48,8 +68,10 @@ const ButtonPanel = ({
 
 ButtonPanel.propTypes = {
   randomize: PropTypes.func.isRequired,
+  insertion: PropTypes.func.isRequired,
   merge: PropTypes.func.isRequired,
   quick: PropTypes.func.isRequired,
+  heap: PropTypes.func.isRequired,
   sorted: PropTypes.bool.isRequired,
 };
 
