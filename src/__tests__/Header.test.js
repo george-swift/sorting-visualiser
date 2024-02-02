b87a05f32570de351a866de03ac991bfe3a32462
George <71434803+george-swift@users.noreply.github.com>
2024-02-02 17:58:03 +0100

    Remove deprecated files

--- src/__tests__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer';
-import Header from '../components/Header';
-
-describe('Snapshot of  the Header component', () => {
-  it('should render correctly', () => {
-    const tree = renderer.create(<Header />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
